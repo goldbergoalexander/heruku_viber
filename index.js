72cb114a64559bb1a04b41591ff66d478de6050c
root <alexandergoldberg@ukr.net>
2018-10-01 13:03:47 +0000

    Add keyboard now

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 'use strict';
-
+const KeyboardGeneratorModule = require('./keyboard_generator.js');
 const ViberBot = require('viber-bot').Bot;
 const BotEvents = require('viber-bot').Events;
 const TextMessage = require('viber-bot').Message.Text;
@@ -30,81 +30,21 @@ function say(response, message) {
     response.send(new TextMessage(message));
 }
 //add keyboard 
-var keyboard = 
-{
-	"Type": "keyboard",
-	"Buttons": [{
-		"Columns": 2,
-		"Rows": 2,
-		"Text": "<br><font color=\"#494E67\"><b>ASIAN</b></font>",
-		"TextSize": "large",
-		"TextHAlign": "center",
-		"TextVAlign": "middle",
-		"ActionType": "reply",
-		"ActionBody": "ASIAN",
-		"BgColor": "#f7bb3f",
-		"Image": "https://s18.postimg.org/9tncn0r85/sushi.png"
-	}, {
-		"Columns": 2,
-		"Rows": 2,
-		"Text": "<br><font color=\"#494E67\"><b>FRENCH</b></font>",
-		"TextSize": "large",
-		"TextHAlign": "center",
-		"TextVAlign": "middle",
-		"ActionType": "reply",
-		"ActionBody": "French",
-		"BgColor": "#7eceea",
-		"Image": "https://s18.postimg.org/ntpef5syd/french.png"
-	}, {
-		"Columns": 2,
-		"Rows": 2,
-		"Text": "<br><font color=\"#494E67\"><b>MEXICAN</b></font>",
-		"TextSize": "large",
-		"TextHAlign": "center",
-		"TextVAlign": "middle",
-		"ActionType": "reply",
-		"ActionBody": "Mexican",
-		"BgColor": "#f6f7f9",
-		"Image": "https://s18.postimg.org/t8y4g4kid/mexican.png"
-	}, {
-		"Columns": 2,
-		"Rows": 2,
-		"Text": "<br><font color=\"#494E67\"><b>ITALIAN</b></font>",
-		"TextSize": "large",
-		"TextHAlign": "center",
-		"TextVAlign": "middle",
-		"ActionType": "reply",
-		"ActionBody": "Italian",
-		"BgColor": "#dd8157",
-		"Image": "https://s18.postimg.org/x41iip3o5/itallian.png"
-	}, {
-		"Columns": 2,
-		"Rows": 2,
-		"Text": "<br><font color=\"#494E67\"><b>INDIE</b></font>",
-		"TextSize": "large",
-		"TextHAlign": "center",
-		"TextVAlign": "middle",
-		"ActionType": "reply",
-		"ActionBody": "Indie",
-		"BgColor": "#f6f7f9",
-		"Image": "https://s18.postimg.org/wq06j3jkl/indi.png"
-	}, {
-		"Columns": 2,
-		"Rows": 2,
-		"Text": "<br><font color=\"#494E67\"><b>MORE</b></font>",
-		"TextSize": "large",
-		"TextHAlign": "center",
-		"TextVAlign": "middle",
-		"ActionType": "reply",
-		"ActionBody": "More",
-		"BgColor": "#a8aaba",
-		"Image": "https://s18.postimg.org/ylmyu98et/more_Options.png"
-	}]
+
+function redeemYesOrNoKeyboard() {
+	let keyboardGenerator = new KeyboardGeneratorModule();
+	keyboardGenerator.addElement('Yes I would',  '#57B8FF');
+	keyboardGenerator.addElement('Not now', '#DB3069');
+	return keyboardGenerator.build();
 }
 
 
-// add keyboard 
 
+// add keyboard 
+function sendQuestion(response) {
+	return response.send(new TextMessage('Would you like to build a bot?',
+		redeemYesOrNoKeyboard()));
+}
 
 
 
@@ -130,13 +70,51 @@ function checkUrlAvailability(botResponse, urlToCheck) {
 
         if (!error && requestResponse.statusCode === 200) {
             if (body.search('is up') !== -1) {
-                say(botResponse, 'ОК! ' + urlToCheck + '. Єтот сайт работает.');
+                				say(botResponse, 'ОК! ' + urlToCheck + '. Єтот сайт работает.');
+			 		
             } else if (body.search('Мда') !== -1) {
                 say(botResponse, 'Емм  ' + urlToCheck + '. Єто не похоже на адрес веб-сайта.? Пожалуйста придерживайтесь формата `test.com`');
+				
+				
+				
+				
+				
+				
             } else if (body.search('down from here') !== -1) {
                 say(botResponse, 'Мда! ' + urlToCheck + '. Не рабочий.');
             } else {
-                say(botResponse, 'Проблемка...Что-то не так с   isup.me.');
+				say(botResponse,'Проблемка...Что-то не так с   isup.me.' );
+				
+			
+			
+			
+			
+			
+			
+			
+			
+				
+				console.log(botResponse.userProfile.name);
+				
+								
+				
+				/*
+				var options = {
+           method: 'POST',
+           url: 'https://chatapi.viber.com/pa/send_message',
+           body: {
+               // some required body properties here
+			   
+               text: 'Welcome to SUSI.AI!, ' + '' + '.',
+               // code for showing the get started button here.
+        }
+           //json: true
+       };
+ */
+        
+	   
+				
+				
             }
         }
     })
@@ -179,7 +157,10 @@ var HEROKU_URL = "https://hidden-harbor-18514.herokuapp.com/";
 
 bot.onTextMessage(/./, (message, response) => {
 	
-	 checkUrlAvailability(keyboard,response, message.text);
+	 //checkUrlAvailability(response, message.text);
+	 
+	 
+	 sendQuestion(response);
            
        
 	
@@ -207,3 +188,6 @@ bot.onTextMessage(/./, (message, response) => {
         process.exit(1);
     });
 }
+
+
+
