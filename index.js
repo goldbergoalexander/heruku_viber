6e2f48afa4b5b3960e8c1cfbf80dcbeefd521df1
root <alexandergoldberg@ukr.net>
2018-12-10 15:09:27 +0000

    add keyboard

--- index.js
+++ index.js
@@ -311,7 +311,9 @@ var keys  = {
 		//console.log('this is response : ' + response);	
 		var name = Object.values(response1.userProfile)[1];
 		//bot.sendMessage(response1.userProfile, new KeyboardMessage(keys));
-		bot.sendMessage(response1.userProfile, new TextMessage('Привіт обери пункт який Вам необхідний '),new KeyboardMessage(keys));
+		bot.sendMessage(response1.userProfile, 
+		new TextMessage('Привіт обери пункт який Вам необхідний ') +
+		new KeyboardMessage(keys));
 		
 		
 	
