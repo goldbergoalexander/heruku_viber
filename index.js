4341bb0eece698b210ae1fc8863b7469cb656fea
root <alexandergoldberg@ukr.net>
2018-12-11 13:07:02 +0000

    add keyboard1

--- index.js
+++ index.js
@@ -225,7 +225,7 @@ function whatyousay(botResponse, urlToCheck) {
 
 
 
-bot.once(TextMessage, (message, response) => {
+bot.on('text', (message, response) => {
 	//bot.on(BotEvents.MESSAGE_RECEIVED,(message, response) => {
 	
 //############### keyboard ######################
