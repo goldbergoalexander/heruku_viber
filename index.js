c527c190a81d45f3c995371bab33f5cc2905171a
root <alexandergoldberg@ukr.net>
2018-12-11 15:21:59 +0000

    add keyboard1

--- index.js
+++ index.js
@@ -382,10 +382,10 @@ bot.onTextMessage(/Transport|Транспорт/, (message,response) => {
 		//
 	say(response, "для отримання данних введівть номер авто ВХХХХХВО  \ud83d\udc47 ").then(() => {
 				
-				 bot.onTextMessage(/./,(mess,response)=>{
+				 bot.on(BotEvents.MESSAGE_RECEIVED,(mess,response)=>{
 					 //console.log("this is mess.text " + " " + mess.text + ' '  +  mess.text.length);
 					 
-                    say(response, " інформація за Вашим номером авто = > " + mess.text.toUpperCase() + " надійде якнайшвидше \ud83d\udd50  ")
+                    say(response, " інформація за Вашим номером авто = > " + mess.text.toUpperCase() + " надійде якнайшвидше \ud83d\udd50  ");
 				 
 				     })
 				 })
