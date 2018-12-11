2182c95ed1bafd1d446d8703abf706434ce6e08d
root <alexandergoldberg@ukr.net>
2018-12-11 13:20:12 +0000

    add keyboard1

--- index.js
+++ index.js
@@ -365,7 +365,7 @@ if (ActionBody==="EDR") {
 		
 	});
 
-bot.onTextMessage(/ПОГОДА||Wheather/, (message, response) => {
+bot.onTextMessage(/ПОГОДА|Wheather/, (message, response) => {
 	
 	say('hey you call weather?');
 })
