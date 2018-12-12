a165675cb4906a48a46619938f27cb66c9b6d4b6
root <alexandergoldberg@ukr.net>
2018-12-12 08:18:42 +0000

    add keyboard1

--- index.js
+++ index.js
@@ -397,7 +397,7 @@ bot.onTextMessage(/Transport|Транспорт/, (message,response) => {   //sd
 	})
 	//#######################################    Curency   ####################################################
 	
-bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
+bot.on(BotEvents.MESSAGE_SEND, (message, response) => {
     // Echo's back the message to the client. Your bot logic should sit here.
     response.send(new TextMessage('hello'));
 });
