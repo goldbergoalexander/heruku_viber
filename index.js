d5a9edca4370fb8c39e3ec24aa5252a839c035c8
root <alexandergoldberg@ukr.net>
2018-12-12 07:18:54 +0000

    add keyboard1

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const ViberBot = require('viber-bot').Bot;
 const BotEvents = require('viber-bot').Events;
 const TextMessage = require('viber-bot').Message.Text;
 const ex = edrModule.search();
-//const BotEvents = require('viber-bot').Events;
+const BotEvents = require('viber-bot').Events;
 var ProgressBar = require('progressbar.js');
 
 
@@ -383,7 +383,8 @@ bot.onTextMessage(/Transport|Транспорт/, (message,response) => {
 		//
 	say(response, "для отримання данних введівть номер авто ВХХХХХВО  \ud83d\udc47 ").then(() => {
 				
-				 bot.on("text",(mess)=>{
+				//bot.on(BotEvents.MESSAGE_RECEIVED
+				 bot.on("text",(messages,response)=>{
 					 //console.log("this is mess.text " + " " + mess.text + ' '  +  mess.text.length);
 					 
                     say(response, " інформація за Вашим номером авто = > " + mess.text.toUpperCase() + " надійде якнайшвидше \ud83d\udd50  ");
