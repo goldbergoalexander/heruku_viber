182bffc38e26416a78bf154d388df0784ad61bed
root <alexandergoldberg@ukr.net>
2018-12-12 08:54:50 +0000

    add keyboard1

--- index.js
+++ index.js
@@ -387,7 +387,7 @@ bot.onTextMessage(/Transport|Транспорт/, (message,response) => {   //sd
 		//bot.sendMessage(response1.userProfile, new TextMessage('Привіт обери пункт який Вам необхідний '));
 	bot.sendMessage(response.userProfile, new TextMessage("для отримання данних введівть номер авто ВХХХХХВО  \ud83d\udc47 "))
 	.then(()=>{
-		say(response, " інформація за Вашим номером авто = > " + messages.text + " надійде якнайшвидше \ud83d\udd50  ");
+		bot.sendMessage(response.userProfile, new TextMessage(message.text));
 				/*
 				bot.on(TextMessage,(message,response)=>{
 				 //bot.on(TextMessage,(messages,response)=>{
