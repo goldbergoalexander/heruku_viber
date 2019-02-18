4e5493fcee40863d91962e73c343cd7b052ec04f
root <alexandergoldberg@ukr.net>
2019-02-18 15:09:20 +0000

    Add test button

--- routes/test_search.js
+++ routes/test_search.js
@@ -60,7 +60,7 @@ MongoClient.connect(url1,/*{ useNewUrlParser: true },*/ function(err, db) {
 	
 	//if (text.length <8)
 	//{
-		dbo.collection('mreotest111').find({"n_reg_new": text1}).toArray()
+		dbo.collection('mreotest111').find({"n_reg_new": new RegExp(text1)}).toArray()
 	.then(data => { 
 	var datas = '';
 for (var i= 0; i< data.length; i++) {
