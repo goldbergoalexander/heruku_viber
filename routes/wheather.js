a0a8c442068afe1c0d28518fad6a94dc5bf79253
root <alexandergoldberg@ukr.net>
2019-03-19 12:58:19 +0000

    change bot on

--- routes/wheather.js
+++ routes/wheather.js
@@ -32,12 +32,12 @@ const bot = new ViberBot(logger, {
 
 
 function wheather (response1){
-	bot.sendMessage(response1,[new TextMessage('Привіт' + ' ' + ' Оберіть за якими параметрами Ви хочете отримати погодні умови ?')/*,new KeyboardMessage(keys)*/]).then(()=>{keyboard.get_keyboard_weather(response1);
+	bot.sendMessage(response1,[new TextMessage('Привіт' + ' ' + ' Оберіть за якими параметрами Ви хочете отримати погодні умови ?')/*,new KeyboardMessage(keys)*/]).then(()=>{keyboard.get_keyboard_weather(response1)
 	
 	bot.onTextMessage(/За один день|oneday/, (message,response) => {
 	bot.sendMessage(response1,[new LocationMessage(latitude, longitude)]).then(()=>{bot.sendMessage(response1,[new TextMessage('Привіт отже твоя локація' + response1.latitude + ' ' + response1.longitude)])  }).catch(err=>{throw err})
 
-	}
+	})
 	
 	
 	}).catch(err=>{throw err})
