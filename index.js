cde172cfa1ca3f6d519edff0a27d68d71477ae49
root <alexandergoldberg@ukr.net>
2019-03-19 14:44:46 +0000

    get location

--- index.js
+++ index.js
@@ -165,6 +165,7 @@ bot.onTextMessage(/main_menu|Головне меню/, (message,response) => {
 })
 bot.onTextMessage(/За один день|oneday/, (message,response) => {  
     var response1 = response.userProfile;
+	console.log('this is response' + " " + response);
 	//bot.sendMessage(response1,[new LocationMessage(latitude, longitude)])
 	bot.sendMessage(response1,[new LocationMessage(response1.latitude, response1.longitude)]).then(()=>{
 	bot.sendMessage(response1,[new TextMessage('Привіт отже твоя локація' + ' ' + response1.latitude + ' ' + response1.longitude)])  
