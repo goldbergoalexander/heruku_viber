9fab9d230d38232a64ecf93a3153ed599f247ba0
root <alexandergoldberg@ukr.net>
2019-03-19 13:08:09 +0000

    change bot on

--- index.js
+++ index.js
@@ -87,12 +87,18 @@ bot.onTextMessage(/ПОГОДА|weather/, (message, response) => {
 	wheather.wheather(response1);
 			//}).cath(err=>{throw err})
 
+			
+			bot.onTextMessage(/За один день|oneday/, (message,response) => {
+	bot.sendMessage(response1,[new LocationMessage(latitude, longitude)]).then(()=>{bot.sendMessage(response1,[new TextMessage('Привіт отже твоя локація' + response1.latitude + ' ' + response1.longitude)])  }).catch(err=>{throw err})
+
+	})
+			
 	//############################################################################################################
 	
 		//bot.sendMessage(response.userProfile, [new TextMessage("для отримання данних оберіть тип способу отримання погоднії умов з меню  \ud83d\udc47 ")/*, new KeyboardMessage(keys_edr)*/])
 	//.then(()=>{
 		//console.log(`${message.latitude}, ${message.longitude}`);
-		keyboard.get_keyboard_weather(response.userProfile);
+		//keyboard.get_keyboard_weather(response.userProfile);
 		
 		//keyboard.get_keyboard_weather(response.userProfile);
 		//####################### take simple search ##############################
