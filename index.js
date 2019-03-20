7744060997c47b7956eda27ffd09e836aeda714b
root <alexandergoldberg@ukr.net>
2019-03-20 09:35:09 +0000

    Get location

--- index.js
+++ index.js
@@ -164,6 +164,8 @@ bot.onTextMessage(/main_menu|Головне меню/, (message,response) => {
 	
 })
 bot.onTextMessage(/./, (message,response) => { 
+console.log('this is response keys from any text' + " " + Object.keys(response));
+console.log('this is response from any text' + " " + Object.values(response));
 console.log('this is message from any text' + " " + Object.values(message));
 console.log('this is message from any text' + " " + Object.keys(message));
 console.log('this is message.trackingData from any text' + " " + Object.keys(message.trackingData));
