8e92d4a3c83bb792cf3406178a030170c7c50d5a
root <alexandergoldberg@ukr.net>
2019-03-21 07:57:21 +0000

    add conf.js

--- routes/keyboards.js
+++ routes/keyboards.js
@@ -3,11 +3,9 @@
 const
 config = require('./conf.js'),
 bot = config.bot,
-TextMessage = require('viber-bot').Message.Text,
-KeyboardMessage = require('viber-bot').Message.Keyboard,
-winston = require('winston'),
-toYAML = require('winston-console-formatter'),
-ViberBot = require('viber-bot').Bot;
+KeyboardMessage = require('viber-bot').Message.Keyboard;
+
+
 
 
 //############### keyboard ######################
