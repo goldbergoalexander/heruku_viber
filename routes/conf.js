d2a6c137de8f020557a5e6744c895aab97f19f68
root <alexandergoldberg@ukr.net>
2019-03-21 07:33:46 +0000

    add conf.js

--- routes/conf.js
+++ routes/conf.js
@@ -1,4 +1,5 @@
 'use strict';
+const winston = require('winston')'
 //############################################################# bot Connect #################################################
 function createLogger() {
     const logger = winston.createLogger({
