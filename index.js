4c210ab4035f8a5bb9189f87ec5ca4fb0b816993
root <alexandergoldberg@ukr.net>
2019-03-21 08:14:12 +0000

    add conf.js

--- index.js
+++ index.js
@@ -348,13 +348,16 @@ bot.onTextMessage(/main_menu|Головне меню/, (message,response) => {
        })
 //#######################################   edrsearch  ####################################################
 
-bot.onTextMessage(/^hi|hello$/i, (message, response) => {
+//bot.onTextMessage(/^hi|hello$/i, (message, response) => {
+bot.onTextMessage(/./i, (message, response) => {
+	if(message.text!=/ПОГОДА|weather/){
+	
     response.send(new TextMessage(`Hi there ${response.userProfile.name}. I am ${bot.name}`)).then(()=>{
 		
 	keyboard.get_keyboard(response.userProfile);	
 		
 	})
-	
+	}
 });
 
 //if (process.env.NOW_URL || process.env.HEROKU_URL) {
